db616fe347df8c19167f2659025099ea5d0ddf72
patrice <patrice@PC-PATRICE>
2022-11-21 18:01:29 +0100

    Ajout des packages:
    bo, dal, bll, servlets, messages
    
    Ajout Context.xml pour définition datasource vers SQLServer

---
+++ src/main/java/fr/eni/javaee/eni_encheres/servlets/CodesResultatServlets.java
@@ -0,0 +1,14 @@
+package fr.eni.javaee.eni_encheres.servlets;
+
+/**
+ * Les codes disponibles sont entre 30000 et 39999
+ */
+public abstract class CodesResultatServlets {
+	
+	/**
+	 * Format Avis.note incorrect
+	 */
+	public static final int FORMAT_AVIS_NOTE_ERREUR=30000;
+	
+	
+}
