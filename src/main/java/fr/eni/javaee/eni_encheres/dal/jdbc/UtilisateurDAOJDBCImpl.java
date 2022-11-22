d6f104f19e6f6a6bc59f1c4cc5d90f2d2573c0d5
patrice <dev@allary-guillery.fr>
2022-11-22 13:58:04 +0100

    Déclaration implémentation interface UtilisateurDAO au lieu de DAO<Utilsateur>

--- src/main/java/fr/eni/javaee/eni_encheres/dal/jdbc/UtilisateurDAOJDBCImpl.java
+++ src/main/java/fr/eni/javaee/eni_encheres/dal/jdbc/UtilisateurDAOJDBCImpl.java
@@ -5,14 +5,16 @@ package fr.eni.javaee.eni_encheres.dal.jdbc;
 
 import java.util.List;
 
+import fr.eni.javaee.eni_encheres.BusinessException;
 import fr.eni.javaee.eni_encheres.bo.Utilisateur;
 import fr.eni.javaee.eni_encheres.dal.DAO;
+import fr.eni.javaee.eni_encheres.dal.UtilisateurDAO;
 
 /**
  * @author patrice
  *
  */
-public class UtilisateurDAOJDBCImpl implements DAO<Utilisateur> {
+public class UtilisateurDAOJDBCImpl implements UtilisateurDAO {
 
 	@Override
 	public void createElement(Utilisateur element) {
@@ -40,6 +42,18 @@ public class UtilisateurDAOJDBCImpl implements DAO<Utilisateur> {
 
 	@Override
 	public List<Utilisateur> getAllElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utilisateur getUtilisateurByPseudo(String pseudo) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utilisateur getUtilisateurByMail(String email) throws BusinessException {
 		// TODO Auto-generated method stub
 		return null;
 	}
