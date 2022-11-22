60a5d1b8a5577313d766c83717e18effc84eb52f
patrice <patrice@PC-PATRICE>
2022-11-22 10:59:24 +0100

    Creation de l'implémentation JDBC Utilisateur

---
+++ src/main/java/fr/eni/javaee/eni_encheres/dal/jdbc/UtilisateurDAOJDBCImpl.java
@@ -0,0 +1,47 @@
+/**
+ * Implémentation JDBC de la DAO Utilisateur
+ */
+package fr.eni.javaee.eni_encheres.dal.jdbc;
+
+import java.util.List;
+
+import fr.eni.javaee.eni_encheres.bo.Utilisateur;
+import fr.eni.javaee.eni_encheres.dal.DAO;
+
+/**
+ * @author patrice
+ *
+ */
+public class UtilisateurDAOJDBCImpl implements DAO<Utilisateur> {
+
+	@Override
+	public void createElement(Utilisateur element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Utilisateur selectElementById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateElement(Utilisateur element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteElementById(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Utilisateur> getAllElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
