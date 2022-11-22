45b7669969a465cc7c597ab560d0118b5a65468c
patrice <patrice@PC-PATRICE>
2022-11-22 10:58:43 +0100

    Création de l'interface DAO Générique.

---
+++ src/main/java/fr/eni/javaee/eni_encheres/dal/DAO.java
@@ -0,0 +1,45 @@
+/**
+ * Interface de definition des méthodes CRUD
+ */
+package fr.eni.javaee.eni_encheres.dal;
+
+import java.util.List;
+
+/**
+ * @author patrice
+ *
+ */
+public interface DAO<T> {
+
+	/**
+	 * Création d'un nouvel élément dans la datasource.
+	 * @param element l'élément à ajouter dans la datasource.
+	 */
+	public void createElement(T element);
+	
+	/**
+	 * Récupère un élément par son identifiant dans la datasource.
+	 * @param id l'identifiant de l'élément à récupérer
+	 * @return l'élément possédant l'identifiant.
+	 */
+	public T selectElementById(int id);
+	
+	/**
+	 * Met à jour un élément dans la datasource.
+	 * @param element l'élément à ajouter.
+	 */
+	public void updateElement(T element);
+	
+	/**
+	 * Supprime un element dans la datasource par son identifiant
+	 * @param id l'identifiant de l'élément à supprimer
+	 */
+	public void deleteElementById(int id);
+	
+	/**
+	 * Récupère la liste de tous les éléments contenus dans la datasource.
+	 * @return tous les élémnts contenus dans la datasource.
+	 */
+	public List<T> getAllElements();
+	
+}
