290d61efbe305e4776aca19db8f3a538e3a1e391
patrice <dev@allary-guillery.fr>
2022-11-22 22:07:26 +0100

    Implémnetation loginManager + cookie authentification ultrabasique

--- src/main/java/fr/eni/javaee/eni_encheres/servlets/Connexion.java
+++ src/main/java/fr/eni/javaee/eni_encheres/servlets/Connexion.java
@@ -5,9 +5,16 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.javaee.eni_encheres.BusinessException;
+import fr.eni.javaee.eni_encheres.bll.CodesResultatBLL;
+import fr.eni.javaee.eni_encheres.bll.LoginManager;
+import fr.eni.javaee.eni_encheres.bll.UtilisateurManager;
+import fr.eni.javaee.eni_encheres.messages.LecteurMessage;
 
 /**
  * Servlet implementation class Connexion
@@ -20,16 +27,47 @@ public class Connexion extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/Connexion.jsp");
-		requestDispatcher.forward(request, response);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/Connexion.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		
+		String identifiant = request.getParameter("identifiant");
+		String password = request.getParameter("password");
+		Cookie authCookie = null;
+		
+		Cookie[] cookies = request.getCookies();
+		for(int i = 0; i < cookies.length; i++) {
+			if( cookies[i].getName().equalsIgnoreCase("authenticated")){
+				authCookie = cookies[i];
+			}
+		}
+		
+		try {
+			boolean authStatus = LoginManager.getInstance().authenticateUser(identifiant, password);
+			if(authStatus) {
+				if(authCookie !=  null) {
+					this.getServletContext().getRequestDispatcher("/WEB-INF/Home.jsp").forward(request, response);
+				} else {
+					authCookie = new Cookie("authenticated", "true");
+					response.addCookie(authCookie);
+					this.getServletContext().getRequestDispatcher("/WEB-INF/Home.jsp").forward(request, response);
+				}
+				
+			} else {
+				request.setAttribute("errorMessage", LecteurMessage.getMessageErreur(CodesResultatServlets.USERNAME_OR_PASSWORD_INVALID));
+				this.getServletContext().getRequestDispatcher("/WEB-INF/Connexion.jsp").forward(request, response);
+
+			}
+		} catch (BusinessException e) {
+			
+			e.printStackTrace();			
+		}
+		
+		
 	}
 
 }
