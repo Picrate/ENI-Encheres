df09d76bf490800e7da5f868e25293d379faabd5
Patrice <dev@allary-guillery.fr>
2022-11-24 09:24:18 +0100

    correction suite push

--- src/main/java/fr/eni/javaee/eni_encheres/dal/DAOFactory.java
+++ src/main/java/fr/eni/javaee/eni_encheres/dal/DAOFactory.java
@@ -1,9 +1,7 @@
 package fr.eni.javaee.eni_encheres.dal;
 
-<<<<<<< HEAD
 import fr.eni.javaee.eni_encheres.bo.Utilisateur;
 import fr.eni.javaee.eni_encheres.dal.jdbc.ArticleDAOJDBCImpl;
->>>>>>> 933b90975f2238412e16fdd0980568855e152269
 import fr.eni.javaee.eni_encheres.dal.jdbc.UtilisateurDAOJDBCImpl;
 
 public abstract class DAOFactory {
