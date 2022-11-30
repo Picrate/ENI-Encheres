ce9df4ac6e491cfb870b4a5d6317e46703f9dd01
Patrice <dev@allary-guillery.fr>
2022-11-30 16:42:48 +0100

    Pojo Enchere

--- src/main/java/fr/eni/javaee/eni_encheres/bo/Enchere.java
+++ src/main/java/fr/eni/javaee/eni_encheres/bo/Enchere.java
@@ -4,7 +4,8 @@
 package fr.eni.javaee.eni_encheres.bo;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Orel
@@ -15,8 +16,10 @@ public class Enchere implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	Date dateEnchere;
+	LocalDateTime dateEnchere;
 	int montantEnchere;
+	int no_utilisateur;
+	int no_article;
 
 	/**
 	 * 
@@ -28,22 +31,35 @@ public class Enchere implements Serializable {
 	 * @param dateEnchere
 	 * @param montantEnchere
 	 */
-	public Enchere(Date dateEnchere, int montantEnchere) {
+	public Enchere(int no_utilisateur, int no_article, LocalDateTime dateEnchere, int montantEnchere) {
+		this.no_utilisateur = no_utilisateur;
+		this.no_article = no_article;
 		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+	
+	/**
+	 * @param dateEnchere
+	 * @param montantEnchere
+	 */
+	public Enchere(int no_utilisateur, int no_article, int montantEnchere) {
+		this.no_utilisateur = no_utilisateur;
+		this.no_article = no_article;
+		this.dateEnchere = LocalDateTime.now();
 		this.montantEnchere = montantEnchere;
 	}
 
 	/**
 	 * @return the dateEnchere
 	 */
-	public Date getDateEnchere() {
+	public LocalDateTime getDateEnchere() {
 		return dateEnchere;
 	}
 
 	/**
 	 * @param dateEnchere the dateEnchere to set
 	 */
-	public void setDateEnchere(Date dateEnchere) {
+	public void setDateEnchere(LocalDateTime dateEnchere) {
 		this.dateEnchere = dateEnchere;
 	}
 
@@ -63,7 +79,23 @@ public class Enchere implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Enchere [dateEnchere=" + dateEnchere + ", montantEnchere=" + montantEnchere + "]";
+		return "Enchere [dateEnchere=" + dateEnchere.format(DateTimeFormatter.ISO_DATE_TIME) + ", montantEnchere=" + montantEnchere + "]";
+	}
+
+	public int getNo_utilisateur() {
+		return no_utilisateur;
+	}
+
+	public int getNo_article() {
+		return no_article;
+	}
+
+	public void setNo_utilisateur(int no_utilisateur) {
+		this.no_utilisateur = no_utilisateur;
+	}
+
+	public void setNo_article(int no_article) {
+		this.no_article = no_article;
 	}
 
 }
