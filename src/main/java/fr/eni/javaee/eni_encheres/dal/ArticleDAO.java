2fd0eb6bcf4f627d959fdce7ca3340c23a53f6fc
VincentFily <vincentfily.code@gmail.com>
2022-12-01 16:07:53 +0100

    DAO Article

--- src/main/java/fr/eni/javaee/eni_encheres/dal/ArticleDAO.java
+++ src/main/java/fr/eni/javaee/eni_encheres/dal/ArticleDAO.java
@@ -7,9 +7,42 @@ import fr.eni.javaee.eni_encheres.bo.Article;
 import fr.eni.javaee.eni_encheres.bo.Enchere;
 
 public interface ArticleDAO extends DAO<Article> {
+	/**
+	 * 
+	 * @param idCategorie
+	 * @return
+	 * @throws BusinessException
+	 */
 	public List<Article> getArticlesInCategorie(int idCategorie) throws BusinessException;
+	
+	/**
+	 * 
+	 * @param idUser
+	 * @return
+	 * @throws BusinessException
+	 */
 	public List<Article> getUserArticlesEnchere(int idUser) throws BusinessException;
+	
+	/**
+	 * 
+	 * @param userId
+	 * @throws BusinessException
+	 */
 	public void deleteAllArticlesByUserId(int userId) throws BusinessException;
+	
+	/**
+	 * 
+	 * @param idUser
+	 * @return
+	 * @throws BusinessException
+	 */
 	public List<Article> getUserWinArticle(int idUser) throws BusinessException;
+	
+	/**
+	 * 
+	 * @param idUser
+	 * @return
+	 * @throws BusinessException
+	 */
 	public List<Article> getEndedSells(int idUser) throws BusinessException;
 }
