61b9d339d1c7996f5a60769d48a487f45fe4d222
VincentFily <vincentfily.code@gmail.com>
2022-12-01 12:02:33 +0100

    modification des données inserees

--- src/main/webapp/assets/sql/03-INSERT-DATAS.sql
+++ src/main/webapp/assets/sql/03-INSERT-DATAS.sql
@@ -24,29 +24,31 @@ INSERT INTO CATEGORIES (libelle) VALUES ('Multimedia');
 INSERT INTO CATEGORIES (libelle) VALUES ('Mode');
 -- Insert objets
 INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie)
-VALUES ('Vélo','Superbe vélo tout neuf','20221110', '20221010',50,0,1,1);
+VALUES ('Vélo','Superbe vélo tout neuf','2022-10-10 20:30:00','2022-11-10 20:30:00',50,0,1,2);
 INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie)
-VALUES ('Ordinateur Windows','Le PC fonctionne mais Eclipse ne veut plus démarrer','20221102','20221202',70,0,1,3);
+VALUES ('Ordinateur Windows','Le PC fonctionne mais Eclipse ne veut plus démarrer','2022-11-02 20:30:00','2022-12-02 20:30:00',70,0,1,3);
 INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie)
-VALUES ('Pantalon','Quelques trous, couture à prévoir','20221113','20221213',5,0,1,4);
+VALUES ('Pantalon','Quelques trous, couture à prévoir','2022-11-13 20:30:00','2022-12-13 20:30:00',5,0,1,4);
 INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie)
-VALUES ('Casquette','Belle casquette rouge, bleu, noir, vert, jaune','20221215','20221215',10,0,1,4);
+VALUES ('Casquette','Belle casquette rouge, bleu, noir, vert, jaune','2022-12-15 20:30:00','2022-12-15 20:30:00',10,0,1,4);
 INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie)
-VALUES ('Téléphone','Marque Alcatel','20221101','20221215',2,0,3,3);
+VALUES ('Téléphone','Marque Alcatel','2022-11-01 20:30:00','2022-12-15 20:30:00',2,0,2,3);
 INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie)
-VALUES ('Lampe','Fonctionne très bien','20221109','20221209',10,0,2,1);
+VALUES ('Lampe','Fonctionne très bien','2022-11-09 20:30:00','2022-12-09 20:30:00',10,0,2,1);
 INSERT INTO ARTICLES_VENDUS (nom_article,description,date_debut_encheres,date_fin_encheres,prix_initial,prix_vente,no_utilisateur,no_categorie)
-VALUES ('Armoire','Un peu lourde','20221203','20221203',50,0,3,1);
+VALUES ('Armoire','Un peu lourde','2022-12-03 20:30:00','2022-12-25 20:30:00',50,0,3,1);
 -- Insert enchère
+-- Objet : Vélo ; Vendeur : patrice ; Acheteur gagnant : vincent
 INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
-VALUES ('2','7','2022-12-20 20:30:00', 55);
-INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
-VALUES ('1','7','2022-12-20 20:30:00', 105);
-INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
-VALUES ('2','1','2022-11-10 18:30:00', 75);
+VALUES ('2','1','2022-10-10 18:30:00', 105);
 INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
 VALUES ('3','1','2022-11-10 18:30:00', 85);
+
 INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
 VALUES ('2','5','2022-11-10 18:30:00', 25);
 INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
 VALUES ('3','5','2022-11-10 18:30:00', 30);
+INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
+VALUES ('2','7','2022-12-20 20:30:00', 55);
+INSERT INTO ENCHERES (no_utilisateur,no_article,date_enchere,montant_enchere)
+VALUES ('1','7','2022-12-20 20:30:00', 105);
